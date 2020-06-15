942fecdd34bfda728ffe170510030e08f4b99b2d
ArsenicMeatball <jeandavid.rousseau@ucalgary.ca>
2020-06-15 11:40:42 -0600

    updated for restriction enzymes

--- biobrick_optimization_tool_synthesis/optimization/logic/test_parameters.py
+++ biobrick_optimization_tool_synthesis/optimization/logic/test_parameters.py
@@ -1,7 +1,8 @@
 from Bio.Alphabet import IUPAC
 from Bio.Seq import Seq
 
-from biobrick_optimization_tool_synthesis.optimization.logic import interpret_sequence
+from biobrick_optimization_tool_synthesis.optimization.logic import cleaning
+from biobrick_optimization_tool_synthesis.optimization.logic.cleaning import get_rest_enzymes_from_string
 from biobrick_optimization_tool_synthesis.optimization.logic.codon_optimization_host import \
     determine_ideal_codon_optimized_sequence
 from biobrick_optimization_tool_synthesis.optimization.logic.set_codon_table import fetch_codon_table
@@ -21,9 +22,10 @@ invalid_rna = valid_rna.replace('U', '=', 3)
 
 unknown = valid_dna.replace('T', 'C')
 
-seq = Seq(interpret_sequence.clean_sequence(valid_protein), IUPAC.protein)
+seq = Seq(cleaning.clean_sequence(valid_protein), IUPAC.protein)
 codon_usage = fetch_codon_table()
 codon_optimized = determine_ideal_codon_optimized_sequence(seq, codon_usage)
+restriction_sites = 'NdeI XhoI HpaI PstI EcoRV NcoI BamHI'
 algorithm_params = {
     'population_size': 5,
     'num_param_considered': 8,
@@ -39,5 +41,5 @@ algorithm_params = {
         'window_size': 20
     },
     'generations': 3,
-    'restriction_sites': 'EcoRI'
+    'restriction_sites': get_rest_enzymes_from_string(restriction_sites)
 }
