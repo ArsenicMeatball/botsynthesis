af097829898e22121ecd30b05f75f1318a593003
ArsenicMeatball <ArsenicMeatball@yandex.com>
2020-08-29 10:44:04 -0600

    added more library functions

--- biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/dict_functions.py
+++ biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/dict_functions.py
@@ -1,5 +1,5 @@
 from biobrick_optimization_tool_synthesis.optimization.codon_opt_synth_spea2.list_functions import \
-    number_of_differences_between_two_lists, differences_between_two_lists
+    differences_between_two_lists
 
 
 def get_differences_dict_list(dict1: dict, dict2: dict) -> dict:
@@ -34,3 +34,32 @@ def get_number_of_differences_dict_list(dict1: dict, dict2: dict) -> int:
     for value in only_diffs.values():
         differences += len(value)
     return differences
+
+
+def find_key_n_minimum_value(d: dict) -> list:
+    """
+    gets minimum value key(s)
+    :param d: dict with values
+    :return: key(s) of minimum
+    """
+    temp = min(d.values())
+    res = [key for key in d if d[key] == temp]
+    return res
+
+
+def sort_dict_by_value_get_list_of_keys(d: dict) -> list:
+    """
+    sorts dict by value and returns list of keys
+    :param d: dict with values
+    :return: keys in order
+    """
+    return sorted(d.keys(), key=d.get)
+
+
+def sort_dict_by_value(d: dict) -> list:
+    """
+    sorts dict by value and returns list of values
+    :param d: dict with values
+    :return: values in order
+    """
+    return sorted(d.values())
