86adc75a1e96667149b83f132dad9125da5ca099
ArsenicMeatball <ArsenicMeatball@yandex.com>
2020-09-17 10:04:31 -0600

    completed BBox for build archive

--- biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/archive.py
+++ biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/archive.py
@@ -1,12 +1,14 @@
 import logging
 
 
-def build_archive(population: dict, archive_size: int, sort_key: str) -> dict:
+def build_archive(population: dict, archive_size: int, sort_key: str, non_dominated_upper_limit: int = 1) -> dict:
     """
     best members of the population go into the archive
     :param archive_size: the size of the archive
     :param population: the union between the population and the "archive"
     :param sort_key: key by which to sort the population
+    :param non_dominated_upper_limit: default 1, all scores below this go into archive automatically,
+        but if size too large, some get truncated
     :return: archive dict of best members for next generation
     """
     logging.info('building archive')
@@ -18,7 +20,7 @@ def build_archive(population: dict, archive_size: int, sort_key: str) -> dict:
     archive = {}
     logging.info('Adding all non_dominated sequences into archive')
     for seq_id in population.keys():
-        if population[seq_id][sort_key] < 1:
+        if population[seq_id][sort_key] < non_dominated_upper_limit:
             archive[seq_id] = population[seq_id]
     # get archive to correct size
     if len(archive) > archive_size:
--- biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/tests/test_archive.py
+++ biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/tests/test_archive.py
@@ -2,27 +2,138 @@ import unittest
 
 from biobrick_optimization_tool_synthesis.optimization.codon_opt_synth_spea2.test_parameters import algorithm_params
 import biobrick_optimization_tool_synthesis.optimization.codon_opt_synth_spea2.fitness_functions as fit_func
+import biobrick_optimization_tool_synthesis.optimization.codon_opt_synth_spea2.archive as arch
+
 
 class TestArchive(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
-    # add dominated to archive black box
-# empty archive, archive size 1, archive smaller than population, archive == population, archive larger than population
-# empty population, population of 1, population smaller than archive, archive == population, population larger than archive
-# archive size of -1, 0, 1, 1<size<size of population, 1<size<size of archive, size = archive, size = population, size > archive, size > population
-# if nothing goes badly, the size of the archive will INCREASE to the archive size
-# else an error should pop up
+        cls.correct_sort_key = '1'
+        cls.not_exist_sort_key = '3'
+        seq1 = {cls.correct_sort_key: 5}
+        seq2 = {cls.correct_sort_key: 4}
+        seq3 = {cls.correct_sort_key: 3}
+        seq4 = {cls.correct_sort_key: 3}
+        seq5 = {cls.correct_sort_key: 2.5}
+        seq6 = {cls.correct_sort_key: 1}
 
-    # truncate archive
+        cls.empty = {}
+        cls.pop_1 = {'1': seq1}
+        cls.pop_2 = {'1': seq1, '2': seq2}
+        cls.pop_3 = {'1': seq1, '2': seq2, '3': seq3}
+        cls.pop_4 = {'1': seq1, '2': seq2, '3': seq3, '4': seq4}
+        cls.pop_5 = {'1': seq1, '2': seq2, '3': seq3, '4': seq4, '5': seq5}
+        cls.pop_6 = {'1': seq1, '2': seq2, '3': seq3, '4': seq4, '5': seq5, '6': seq6}
+        cls.pop_r1 = {'6': seq6}
+        cls.pop_r2 = {'6': seq6, '5': seq5}
+        cls.pop_r3 = {'6': seq6, '5': seq5, '4': seq4}
+        cls.pop_r4 = {'6': seq6, '5': seq5, '4': seq4, '3': seq3}
+        cls.pop_r5 = {'6': seq6, '5': seq5, '4': seq4, '3': seq3, '2': seq2}
+        cls.pop_r6 = {'6': seq6, '5': seq5, '4': seq4, '3': seq3, '2': seq2, '1': seq1}
 
     # build archive black box
-# empty population, population of 1, population of many (5)
-# archive size of -1, 0, 1, between 1 and size of population, size of population, greater than size of population
-# if nothing goes badly, an archive (subset of the population) with size equal to archive size will be returned
-# else descriptive error message
-    def test_build_archive_(self):
-        pass
+    # empty population, population of 1, population of many (5)
+    # archive size of -1, 0, 1, between 1 and size of population,(size of population or greater than size of population)
+    # sort key correct, not existing
+    # if nothing goes badly, an archive (subset of the population) with size equal to archive size will be returned
+    # else descriptive error message
+    def test_build_archive_not_existing(self):
+        pops = [self.pop_5, self.pop_1]
+        for pop in pops:
+            self.assertRaises(KeyError, arch.build_archive(pop, len(pop) + 1, self.not_exist_sort_key))
+            self.assertRaises(KeyError, arch.build_archive(pop, len(pop), self.not_exist_sort_key))
+            self.assertRaises(KeyError, arch.build_archive(pop, len(pop) - 1, self.not_exist_sort_key))
+            self.assertRaises(KeyError, arch.build_archive(pop, 1, self.not_exist_sort_key))
+            self.assertRaises(KeyError, arch.build_archive(pop, 0, self.not_exist_sort_key))
+            self.assertRaises(KeyError, arch.build_archive(pop, -1, self.not_exist_sort_key))
+
+    def test_build_archive_pop5_arch_size_gt_pop(self):
+        archive = arch.build_archive(self.pop_5, len(self.pop_5) + 1, self.correct_sort_key)
+        if len(archive) != len(self.pop_5):
+            self.fail('Incorrect archive size')
+        for x in range(1, 6):
+            if str(x) not in archive:
+                self.fail('Failed to find expected key in result archive')
+
+    def test_build_archive_pop5_arch_size_eq_pop(self):
+        archive = arch.build_archive(self.pop_5, len(self.pop_5), self.correct_sort_key)
+        if len(archive) != len(self.pop_5):
+            self.fail('Incorrect archive size')
+        for x in range(1, 6):
+            if str(x) not in archive:
+                self.fail('Failed to find expected key in result archive')
+
+    def test_build_archive_pop5_arch_size_lt_pop(self):
+        archive = arch.build_archive(self.pop_5, len(self.pop_5) - 1, self.correct_sort_key)
+        if len(archive) != len(self.pop_5) - 1:
+            self.fail('Incorrect archive size')
+        for x in range(2, 6):
+            if str(x) not in archive:
+                self.fail('Failed to find expected key in result archive')
+
+    def test_build_archive_pop5_arch_size_1(self):
+        archive = arch.build_archive(self.pop_5, 1, self.correct_sort_key)
+        if len(archive) != 1:
+            self.fail('Incorrect archive size')
+        if '5' not in archive:
+            self.fail('Failed to find expected key in result archive')
+
+    def test_build_archive_pop5_arch_size_0(self):
+        archive = arch.build_archive(self.pop_5, 0, self.correct_sort_key)
+        if len(archive) != 0:
+            self.fail('Incorrect archive size')
+
+    def test_build_archive_pop5_arch_size_neg_1(self):
+        self.assertRaises(ValueError, arch.build_archive(self.pop_5, -1, self.correct_sort_key))
+
+    def test_build_archive_pop1_arch_size_gt_pop(self):
+        archive = arch.build_archive(self.pop_1, len(self.pop_1) + 1, self.correct_sort_key)
+        if len(archive) != len(self.pop_1):
+            self.fail('Incorrect archive size')
+        if '1' not in archive:
+            self.fail('Failed to find expected key in result archive')
+
+    def test_build_archive_pop1_arch_size_eq_pop(self):
+        archive = arch.build_archive(self.pop_1, len(self.pop_1), self.correct_sort_key)
+        if len(archive) != len(self.pop_1):
+            self.fail('Incorrect archive size')
+        if '1' not in archive:
+            self.fail('Failed to find expected key in result archive')
+
+    def test_build_archive_pop1_arch_size_0(self):
+        archive = arch.build_archive(self.pop_1, 0, self.correct_sort_key)
+        if len(archive) != 0:
+            self.fail('Incorrect archive size')
+
+    def test_build_archive_pop1_arch_size_neg_1(self):
+        self.assertRaises(ValueError, arch.build_archive(self.pop_1, -1, self.correct_sort_key))
+
+    def test_build_archive_empty_arch_size_1(self):
+        archive = arch.build_archive(self.empty, 1, self.correct_sort_key)
+        if len(archive) != 0:
+            self.fail('Expected empty archive')
+
+    def test_build_archive_empty_arch_size_0(self):
+        archive = arch.build_archive(self.empty, 0, self.correct_sort_key)
+        if len(archive) != 0:
+            self.fail('Expected empty archive')
+
+    def test_build_archive_empty_arch_size_neg_1(self):
+        self.assertRaises(ValueError, arch.build_archive(self.empty, -1, self.correct_sort_key))
+
+    # add dominated to archive black box
+    # empty archive, archive size 1, archive smaller than population, archive == population, archive larger than population
+    # empty population, population of 1, population smaller than archive, archive == population, population larger than archive
+    # archive size of -1, 0, 1, 1<size<size of population, 1<size<size of archive, size = archive, size = population, size > archive, size > population
+    # sort key correct, incorrect, not existing
+    # if nothing goes badly, the size of the archive will INCREASE to the archive size
+    # else an error should pop up
+
+    # truncate archive
+    # empty archive, population of 1, population of many (5)
+    # archive size of -1, 0, 1, between 1 and size of archive, size of archive, greater than size of archive
+    # sort key correct, incorrect, not existing
+    # if nothing goes badly, an archive (subset of the population) with size equal to archive size will be returned
 
 
 if __name__ == '__main__':
