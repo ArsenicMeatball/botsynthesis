77063e41548aa64a37024b4fd11b660b1d5ae0d1
ArsenicMeatball <jeandavid.rousseau@ucalgary.ca>
2021-01-19 13:25:01 -0700

    Finished writing archive unit tests

--- biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/archive.py
+++ biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/archive.py
@@ -41,26 +41,29 @@ def add_dominated_to_archive(archive: dict, population: dict, archive_size: int,
     :param sort_key: key by which to sort the population
     :return: None, just updates archive
     """
+    if len(population) < archive_size:
+        raise ValueError('Population is too small to add members to archive until full')
     logging.info('adding best dominated members from population to archive')
     sorted_keys = sorted(population.keys(), key=lambda x: population[x][sort_key])
+    # assuming minima, just add the next best one until we reach desired size
+    # also assumes that archive already contains all the dominated, though we are just adding the next best items
     while len(archive) < archive_size:
         next_item = sorted_keys[len(archive)]
         archive[next_item] = population[next_item]
 
 
-def truncate_archive(archive: dict, archive_size: int, sort_key: str):
+def truncate_archive(archive: dict, desired_archive_size: int, sort_key: str):
     """
     removes worst members from archive until the right size is achieved
-    - expects population size > archive size
     :param archive: the archive we are truncating
-    :param archive_size: the final size we are trying to get to
-    :param sort_key: key by which to sort the population
+    :param desired_archive_size: the final size we are trying to get to
+    :param sort_key: key by which to sort the archive
     :return: None, just updates archive
     """
-    if archive_size > archive_size:
-        logging.warning('Supplied ')
+    if desired_archive_size > len(archive):
+        raise ValueError('Desired archive size greater than given archive')
     logging.info('truncating worst non-dominated members from archive')
     sorted_keys = sorted(archive.keys(), key=lambda x: archive[x][sort_key])
-    while len(archive) > archive_size:
+    while len(archive) > desired_archive_size:
         archive.pop(sorted_keys[-1])
         sorted_keys.pop(-1)
--- biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/tests/test_archive.py
+++ biobrick_optimization_tool_synthesis/optimization/codon_opt_synth_spea2/tests/test_archive.py
@@ -1,5 +1,5 @@
 import unittest
-
+import copy
 import biobrick_optimization_tool_synthesis.optimization.codon_opt_synth_spea2.archive as arch
 
 
@@ -8,27 +8,21 @@ class TestArchive(unittest.TestCase):
     def setUpClass(cls):
         cls.correct_sort_key = '1'
         cls.not_exist_sort_key = 'tronbikesupreme'
-        seq1 = {cls.correct_sort_key: 5}
-        seq2 = {cls.correct_sort_key: 4}
-        seq3 = {cls.correct_sort_key: 3}
-        seq4 = {cls.correct_sort_key: 3}
-        seq5 = {cls.correct_sort_key: 2.5}
-        seq6 = {cls.correct_sort_key: 1}
+        cls.seq1 = {cls.correct_sort_key: 5}
+        cls.seq2 = {cls.correct_sort_key: 4}
+        cls.seq3 = {cls.correct_sort_key: 3}
+        cls.seq4 = {cls.correct_sort_key: 3}
+        cls.seq5 = {cls.correct_sort_key: 2.5}
+        cls.seq6 = {cls.correct_sort_key: 1}
 
         cls.empty = {}
-        cls.pop_1 = {'1': seq1}
-        cls.pop_5 = {'1': seq1, '2': seq2, '3': seq3, '4': seq4, '5': seq5}
-        cls.pop_6 = {'1': seq1, '2': seq2, '3': seq3, '4': seq4, '5': seq5, '6': seq6}
-        cls.pop_r1 = {'6': seq6}
-        cls.pop_r5 = {'6': seq6, '5': seq5, '4': seq4, '3': seq3, '2': seq2}
-        cls.pop_r6 = {'6': seq6, '5': seq5, '4': seq4, '3': seq3, '2': seq2, '1': seq1}
+        cls.pop_1 = {'1': cls.seq1}
+        cls.pop_5 = {'1': cls.seq1, '2': cls.seq2, '3': cls.seq3, '4': cls.seq4, '5': cls.seq5}
+        cls.pop_6 = {'1': cls.seq1, '2': cls.seq2, '3': cls.seq3, '4': cls.seq4, '5': cls.seq5, '6': cls.seq6}
+        cls.pop_r1 = {'6': cls.seq6}
+        cls.pop_r5 = {'6': cls.seq6, '5': cls.seq5, '4': cls.seq4, '3': cls.seq3, '2': cls.seq2}
+        cls.pop_r6 = {'6': cls.seq6, '5': cls.seq5, '4': cls.seq4, '3': cls.seq3, '2': cls.seq2, '1': cls.seq1}
 
-    # build archive black box
-    # empty population, population of 1, population of many (5)
-    # archive size of -1, 0, 1, between 1 and size of population,(size of population or greater than size of population)
-    # sort key correct, not existing
-    # if nothing goes badly, an archive (subset of the population) with size equal to archive size will be returned
-    # else descriptive error message
     def test_build_archive_not_existing(self):
         pops = [self.pop_5, self.pop_1]
         for pop in pops:
@@ -110,24 +104,46 @@ class TestArchive(unittest.TestCase):
     def test_build_archive_empty_arch_size_neg_1(self):
         self.assertRaises(IndexError, arch.build_archive, self.empty, -1, self.correct_sort_key)
 
-    # add dominated to archive black box
-    # empty archive, archive size 1, archive smaller than population, archive == population, archive larger than population
-    # empty population, population of 1, population smaller than archive, archive == population, population larger than archive
-    # archive size of -1, 0, 1, 1<size<pop size <= arch size, 1<size<arch size <= pop size, size = archive, size = population, size > archive, size > population
-    # sort key correct, not existing
-    # if nothing goes badly, the size of the archive will INCREASE to the archive size
-    # else an error should pop up
-    # def test_add_dominated_to_archive_arch5_pop1_arch_size_gt_pop
     def test_add_dominated_to_archive_pass(self):
-        pass
+        # should add the best dominated members from the population into the archive
+        expected = {'5': self.seq5, '6': self.seq6, '4': self.seq4, '3': self.seq3}
+        actual = {'5': self.seq5, '6': self.seq6}
+        pop = copy.deepcopy(self.pop_6)
+        desired_size = 4
+        arch.add_dominated_to_archive(actual, pop, desired_size, self.correct_sort_key)
+        self.assertEqual(expected, actual, 'Partial-Did not result in the same dict')
+        # same thing but with an empty dict
+        actual = {}
+        arch.add_dominated_to_archive(actual, pop, desired_size, self.correct_sort_key)
+        self.assertEqual(expected, actual, 'Empty-Did not result in the same dict')
 
-    # truncate archive
-    # empty archive, population of 1, population of many (5)
-    # archive size of -1, 0, 1, between 1 and size of archive, size of archive, greater than size of archive
-    # sort key correct, not existing
-    # if nothing goes badly, an archive (subset of the population) with size equal to archive size will be returned
-    def test_truncate_archive(self):
-        pass
+    def test_add_dominated_to_archive_fail(self):
+        # population too small
+        archive = population = {}
+        desired_size = 10000000
+        self.assertRaises(ValueError, arch.add_dominated_to_archive, archive, population, desired_size, self.correct_sort_key)
+        # wrong key
+        self.assertRaises(KeyError, arch.add_dominated_to_archive, archive, self.pop_6, 1, self.not_exist_sort_key)
+
+    def test_truncate_archive_pass(self):
+        # should remove worst (highest scores) until correct size
+        actual = copy.deepcopy(self.pop_6)
+        desired_size = 0
+        expected = {}
+        arch.truncate_archive(actual, desired_size, self.correct_sort_key)
+        self.assertEqual(expected, actual, 'Can not truncate archive down to 0')
+        actual = copy.deepcopy(self.pop_6)
+        desired_size = 4
+        expected = {'5': self.seq5, '6': self.seq6, '4': self.seq4, '3': self.seq3}
+        arch.truncate_archive(actual, desired_size, self.correct_sort_key)
+        self.assertEqual(expected, actual, 'Can not truncate archive down to regular case')
+
+    def test_truncate_archive_fail(self):
+        # archive smaller than desired size
+        self.assertRaises(ValueError, arch.truncate_archive, {}, 10000, self.correct_sort_key)
+        # wrong key
+        self.assertRaises(KeyError, arch.truncate_archive, self.pop_6, 0, self.not_exist_sort_key)
+
 
 if __name__ == '__main__':
     unittest.main()
